aa59b5b8627ba97caab5d03d352c55dcc7824f7e
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-13 22:59:57 +0600

    11/12/24

--- tip_calculator.py
+++ tip_calculator.py
@@ -2,6 +2,10 @@
 print("welcome to the tip calculator.")
 bil=float(input("What was the total bill? $ "))
 tip=int(input("What percentage tip would you like to give? 10, 12 or 15? "))
-count=int(input("How many people to split the bill? "))
+people=int(input("How many people to split the bill? "))
 bil_tip=tip/100
-tp=bil*bil_tip
+tpa=bil*bil_tip
+tb=bil+tpa
+bilpp=tb/people
+final=round(bilpp,2)
+print(f"Each person should pay $ {final}")
