2296f3072ecae2125ae89b8d4a7a989745fa7dde
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-15 02:17:08 +0600

    11/14/24

--- treasure_Island.py
+++ treasure_Island.py
@@ -27,6 +27,18 @@ c1=input('You\'re at a crossroad, where do you want to go? Type "left" or "right
 
 if c1=="left":
     c2=input('"You\'ve cone to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
+    if c2=="wait":
+        c3=input("You arrive at the island unharead. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?").lower()
+        if c3=="red":
+            print("It's a room full of fire. Game Over.")
+        elif c3=="yellow":
+            print("You found the treasure! You Win")
+        elif c3=="blue":
+            print("You enter a room of beast. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You got attacked by an angry trout. Game Over.")
 else:
     print("You fell into a hole. Game Over.")
 
