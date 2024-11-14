10b661526b8422c2cdeeed4d2d0f67db09ddeba1
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-15 02:19:49 +0600

    11/14/24

--- treasure_Island.py
+++ treasure_Island.py
@@ -44,5 +44,4 @@ else:
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
-#Code is written from here 👇
 
